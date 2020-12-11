863b8538e759fed7ac4bb8a6afd0739619aecc21
Scormit <sbenitezr@outlook.com>
2020-12-10 19:00:27 -0600

    Inserte Imagen en ScrollPage 1

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -15,10 +15,53 @@ class ScrollPage extends StatelessWidget {
   }
 
   Widget _pagina1(){
-    return Center(
-      child: Text('ScrollPage 1'),
+    return Stack(
+      children: <Widget>[
+        _colorFondo(),
+        _imagenFondo(),
+        _textos(),
+      ],
     );
   }
+
+  Widget _colorFondo(){
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(80, 194, 221, 1.0),
+    );
+  }
+
+  Widget _imagenFondo(){
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage('assets/scroll-1.png'),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _textos(){
+
+    final estiloTexto = TextStyle(color: Colors.white, fontSize: 50.0);
+    return SafeArea(
+          child: Column(
+            children: <Widget>[
+            SizedBox(height:20.0),
+            Text('11',style:estiloTexto),
+            Text('Miércoles',style:estiloTexto),
+            Expanded(child: Container(),),
+            Icon(Icons.keyboard_arrow_down, size:70.0, color: Colors.white,)
+          ],
+      ),
+    );
+  }
+
+
+
+
 
   Widget _pagina2(){
     return Center(
