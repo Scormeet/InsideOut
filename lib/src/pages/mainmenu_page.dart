b75a767d06d408e10861a1a928c9271e62ce76a0
Scormit <sbenitezr@outlook.com>
2020-12-17 18:04:10 -0600

    Diseño Main Menu

--- lib/src/pages/mainmenu_page.dart
+++ lib/src/pages/mainmenu_page.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -120,52 +121,52 @@ class MainMenuPage extends StatelessWidget {
       children: [
         TableRow(
           children: [
-            _crearBotonRedondeado(),
-            _crearBotonRedondeado(),
+            _crearBotonRedondeado(Colors.blue, Icons.border_all, 'General'),
+            _crearBotonRedondeado(Colors.purpleAccent, Icons.directions_bus, 'Bus'),
           ]
         ),
         TableRow(
           children: [
-            _crearBotonRedondeado(),
-            _crearBotonRedondeado(),
+            _crearBotonRedondeado(Colors.pinkAccent, Icons.shop, 'Comprar'),
+            _crearBotonRedondeado(Colors.orange, Icons.insert_drive_file, 'Documentos'),
           ]
         ),
         TableRow(
           children: [
-            _crearBotonRedondeado(),
-            _crearBotonRedondeado(),
-          ]
-        ),
-        TableRow(
-          children: [
-            _crearBotonRedondeado(),
-            _crearBotonRedondeado(),
+            _crearBotonRedondeado(Colors.blueAccent, Icons.movie_filter, 'Entretenimiento'),
+            _crearBotonRedondeado(Colors.greenAccent, Icons.cloud, 'Tienda'),
           ]
         )
       ],
     );
   }
 
-  Widget _crearBotonRedondeado(){
-    return Container(
-      height: 180.0,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: Color.fromRGBO(62, 66, 107, 0.70),
-        borderRadius: BorderRadius.circular(20.0)
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          SizedBox(height: 5.0),
-          CircleAvatar(
-            backgroundColor: Colors.pink,
-            radius: 35.0,
-            child: Icon(Icons.access_alarms, color: Colors.white, size: 30.0)
+  Widget _crearBotonRedondeado(Color color, IconData icono, String texto){
+    
+    return ClipRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 7.0, sigmaY: 7.0),
+        child: Container(
+          height: 180.0,
+          margin: EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(62, 66, 107, 0.70),
+            borderRadius: BorderRadius.circular(35.0)
           ),
-          Text('XD', style: TextStyle(color: Colors.pink),),
-          SizedBox(height: 5.0),
-        ],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              SizedBox(height: 5.0),
+              CircleAvatar(
+                backgroundColor: color,
+                radius: 35.0,
+                child: Icon(icono, color: Colors.white, size: 30.0)
+              ),
+              Text(texto, style: TextStyle(color: color),),
+              SizedBox(height: 5.0),
+            ],
+          ),
+        ),
       ),
     );
   }
